5045e9c3306b8070a8793fa52967a19ce8b91f83
dott2019 <49723025+dott2019@users.noreply.github.com>
2019-08-19 11:59:30 +0200

    Add files via upload

---
+++ Col.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+export class Col extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <td>Test 1</td>
+                <td>Test 2</td>
+            </Fragment>
+        )
+    }
+}
+
+export default Col
